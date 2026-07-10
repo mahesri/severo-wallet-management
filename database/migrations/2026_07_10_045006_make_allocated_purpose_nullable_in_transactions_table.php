4eeca468aad73cf71bfe486087d07c3ce133b030
mahesri <adi504097@gmail.com>
2026-07-10 11:55:42 +0700

    adding resource

--- database/migrations/2026_07_10_045006_make_allocated_purpose_nullable_in_transactions_table.php
+++ database/migrations/2026_07_10_045006_make_allocated_purpose_nullable_in_transactions_table.php
@@ -6,22 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::table('transactions', function (Blueprint $table) {
+            $table->enum('allocated_purpose', ['gold', 'btc','saving_for_married'])->nullable()->change();
             $table->integer('allocate')->nullable()->change();
-            $table->enum('allocate_purpose', ['gold', 'btc','saving_for_married'])->nullable()->change();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        //
+        Schema::table('transactions', function (Blueprint $table) {
+            $table->enum('allocated_purpose', ['gold', 'btc','saving_for_married'])->nullable(false)->change();
+            $table->integer('allocate')->nullable(false)->change();
+        });
     }
 };
